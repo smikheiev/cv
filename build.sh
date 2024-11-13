91f236c258bedf5c50f44deb285c46aa3c0f12b4
Serhii Mikheiev <mikh.s.o@gmail.com>
2024-11-14 00:33:01 +0200

    lock version for build tools

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-npx --yes mustache src/data.json src/template.html > output/cv.html
-npx --yes html-export-pdf-cli output/cv.html --page-size A4 --additional-styles src/pdfExportStyles.css --margin top=40,bottom=40,left=40,right=40 -o output/cv.pdf
+npx --yes mustache@4.2.0 src/data.json src/template.html > output/cv.html
+npx --yes html-export-pdf-cli@1.2.0 output/cv.html --page-size A4 --additional-styles src/pdfExportStyles.css --margin top=40,bottom=40,left=40,right=40 -o output/cv.pdf
