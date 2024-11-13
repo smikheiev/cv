95ba201c6cc2097c06f5566be1f42fc8e51c3d58
Serhii Mikheiev <mikh.s.o@gmail.com>
2024-11-14 00:29:25 +0200

    move outer padding for pdf from css styles to export arguments

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npx --yes mustache src/data.json src/template.html > output/cv.html
-npx --yes html-export-pdf-cli output/cv.html --page-size A4 -o output/cv.pdf
+npx --yes html-export-pdf-cli output/cv.html --page-size A4 --additional-styles src/pdfExportStyles.css --margin top=40,bottom=40,left=40,right=40 -o output/cv.pdf
